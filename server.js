eecb2506b22f489f91ef8e7d0efa1119f66b7994
Tharun sekar <tharunsd23@gmail.com>
2021-01-16 01:32:36 +0530

    blog

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const articleRouter = require("./routes/articles")
+const methodOverride = require('method-override')
+const app = express();
+const mongoose = require("mongoose")
+const Article = require("./models/articles")
+app.set("view engine", "ejs" )
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+app.use("/articles",articleRouter)
+
+mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true
+})
+app.get("/",async(req,res) => {
+    const articles = await Article.find().sort({createdAt:'desc'}) 
+    res.render("articles/index",{articles: articles})
+})
+
+app.listen(5000)
